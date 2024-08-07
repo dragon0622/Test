73cd9e1b5896b28c5e060101ede8ee271d26b0fa
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2024-08-07 12:06:26 +0900

    Add files via upload

---
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/ProductClient.java
@@ -0,0 +1,11 @@
+package com.sparta.msa_exam.order;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("product-service")
+public interface ProductClient {
+    @GetMapping("/product/{id}")
+    String getProduct(@PathVariable("id") String id);
+}
